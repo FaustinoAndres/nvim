b8c6c2d178c529bd6982dd7fc4777c18dc27bb73
Faustino Correa <faustino.correa.m@gmail.com>
2024-09-23 16:18:04 -0300

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,6 @@ return {
     priority = 1000,
     config = function()
         vim.cmd.colorscheme("catppuccin")
-        -- vim.o.background("dark")
+        vim.o.background = "dark"
     end,
 }
